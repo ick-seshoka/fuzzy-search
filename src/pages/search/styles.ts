696cb4e31687732d9aed585e03dcfb9afecd5806
Isaac Seshoka <ick.seshoka@gmail.com>
2023-04-05 19:17:55 +0200

    wip(search-ui): initialize search page component, add styled components types and clean up unused css

---
+++ src/pages/search/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-width: 100vw;
+  min-height: 100vh;
+  background-color: #3b3ddd;
+`;
